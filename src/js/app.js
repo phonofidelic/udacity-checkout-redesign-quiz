51e24f309705194ff0e6308a722e74df6202bcbf
phonofidelic <chris_clemons85@hotmail.com>
2016-06-13 06:37:34 -0400

    Updated data model. Each is now an object containing value, status and error (if one exists)

--- src/js/app.js
+++ src/js/app.js
@@ -27,31 +27,52 @@ app.controller('checkoutController', ['$scope', 'IssueTracker', function($scope,
         var validate = function(input) {
             for (i = 0; i < input.length; i++) {
                 var dataKey = input[i].name.replace(/\s+/g, '_');
+                // create object to hold data for each input element
+                data[dataKey] = {};
+                // check for empty inputs
                 if (input[i].value === "") {
                     issueTracker.add("Please enter your " + input[i].name);
+                    data[dataKey].message = ("Please enter your " + input[i].name);
+                // TODO: check for illegal characters
                 } else {
-                    data[dataKey] = input[i].value;
+                    // create key/value pair for input value
+                    data[dataKey].value = input[i].value;
+                    data[dataKey].status = 'has-success';
                 }
             }
-
         }
         validate(inputArr);
 
+        // console.log('inputArr: ', inputArr);
         console.log('data: ', data, Object.keys(data).length);
         console.log('checkValidation', issueTracker.issues);
 
         var inputIssues = issueTracker.retrieve();
         console.log('issueTracker.retrieve(): ', inputIssues);
+        console.log('status: ', data.name.status);
     };
 
     $scope.progressBar = function() {
         var progress = 0;
-        Object.keys($scope.data).forEach(function() {
-            progress += 10;
-            $('.progress-bar').css('width', (progress+'%'));
-        });
+        if (Object.keys($scope.data)) {
+            Object.keys($scope.data).forEach(function() {
+                progress += 10;
+                $('.progress-bar')
+                    .css('width', (progress+'%'))
+                    .attr('aria-valuenow', progress);
+            });
+        }
         console.log('progress: ', progress);
     };
+
+    $scope.inputFeedback = function(node) {
+        console.log('test: ', node.data.status);
+        // for (input in data) {
+        //     if (input.status) {
+        //         return $('/* divNode */').addClass()
+        //     }
+        // }
+    }
 }]);
 
 /* IssueTracker Service */
